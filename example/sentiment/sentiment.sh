ef3043bcd607f5a25abfe02917a40b9c3749000c
Adam Mathias Bittlingmayer <bittlingmayer@users.noreply.github.com>
2017-05-27 03:53:47 -0700

    Update sentiment.sh

--- example/sentiment/sentiment.sh
+++ example/sentiment/sentiment.sh
@@ -1,4 +1,4 @@
-./fasttext supervised -input data/amazon_reviews/train.ft.txt -output model_amzn
+./fasttext supervised -input "@1" -output model_amzn
 
-./fasttext test -model model_amzn.bin data/amazon_reviews/test.ft.txt
+./fasttext test -model model_amzn.bin "@2"
 
